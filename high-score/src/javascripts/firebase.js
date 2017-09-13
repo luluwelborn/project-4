cfe5b63fe89c895f8829034d2e51134b652a72fe
laurenwelborn <lulu.welborn@gmail.com>
2017-09-13 14:55:24 -0600

    complete working shell of react app

--- high-score/src/javascripts/firebase.js
+++ high-score/src/javascripts/firebase.js
@@ -1,18 +1,18 @@
-// import * as firebase from 'firebase'
-// let database
+import * as firebase from 'firebase'
+let database
 
-// export const init = () => {
-//   let config = {
-//     apiKey: "AIzaSyCH0dx7A6nicpyFz4BOKm1lNNUcCYjUk0E",
-//     authDomain: "high-score-app-3c45c.firebaseapp.com",
-//     databaseURL: "https://high-score-app-3c45c.firebaseio.com",
-//     projectId: "high-score-app-3c45c",
-//     storageBucket: "high-score-app-3c45c.appspot.com",
-//     messagingSenderId: "671207886221"
-//   }
-//   firebase.initializeApp(config)
-//   database = firebase.database()
-// }
+export const init = () => {
+  let config = {
+    apiKey: "AIzaSyCH0dx7A6nicpyFz4BOKm1lNNUcCYjUk0E",
+    authDomain: "high-score-app-3c45c.firebaseapp.com",
+    databaseURL: "https://high-score-app-3c45c.firebaseio.com",
+    projectId: "high-score-app-3c45c",
+    storageBucket: "high-score-app-3c45c.appspot.com",
+    messagingSenderId: "671207886221"
+  }
+  firebase.initializeApp(config)
+  database = firebase.database()
+}
 
 // SCRIPT FOR FIREBASE INITIALIZATION
 // <script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
