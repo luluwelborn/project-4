a893701cd962f5834b86e1e7ed5a8fd00330ec3c
laurenwelborn <lulu.welborn@gmail.com>
2017-09-17 18:35:59 -0600

    removed some bad script tags

--- high-score/public/app.js
+++ high-score/public/app.js
@@ -1,82 +1,62 @@
+// THROWING GET ERROR
 var myGames = new Firebase('https://high-score-app-3c45c.firebaseapp.com/');
 
 
-// ------ SAVE GAME ------
+// ------ GET GAME ON PAGE ------
+// function saveToList(event) {
+//     if (event.which == 13 || event.keyCode == 13) { // save the data
+//         var gameName = document.getElementById('gameName').value.trim();
+//         if (gameName.length > 0) {
+//             var li = '<li>' + gameName + '</li>';
+//             document.getElementById('myGames').innerHTML += li;
+//         }
+//         document.getElementById('gameName').value = '';
+//         return false;
+//     }
+// }
+
+// ------ GET - SAVE GAME ------
 function saveToList(event) {
-    if (event.which == 13 || event.keyCode == 13) { // enter key will attempt to save the data
+    if (event.which == 13 || event.keyCode == 13) { // enter key will saves the data
         var gameName = document.getElementById('gameName').value.trim();
         if (gameName.length > 0) {
-            var li = '<li>' + gameName + '</li>';
-            document.getElementById('myGames').innerHTML += li;
+            saveToFB(gameName);
         }
         document.getElementById('gameName').value = '';
         return false;
     }
 }
  
-// // ------ SAVE GAME TO DB ------
-// function saveToFB(gameName) {
-//     myGames.push({
-//         name: gameName
-//     });
-// };
+// ------ PUSH - SAVE GAME TO DB ------
+function saveToFB(gameName) {
+    myGames.push({
+        name: gameName
+    });
+};
 
-// function refreshUI(list) {
-//     var list = '';
-//     for (var i = 0; i < list.length; i++) {
-//         list += '<li data-key="' + list[i].key + '">' + list[i].name + ' [' + genLinks(list[i].key, list[i].name) + ']</li>';
-//     };
-//     document.getElementById('myGames').innerHTML = list;
-// };
+function refreshUI(list) {
+    var lis = '';
+    for (var i = 0; i < list.length; i++) {
+        lis += '<li data-key="' + list[i].key + '">' + list[i].name + '</li>';
+    };
+    document.getElementById('myGames').innerHTML = lis;
+};
 
-// function genLinks(key, gmName) {
-//     var links = '';
-//     links += '<a href="javascript:edit(\'' + key + '\',\'' + gmName + '\')">Edit</a> | ';
-//     links += '<a href="javascript:del(\'' + key + '\',\'' + gmName + '\')">Delete</a>';
-//     return links;
-// };
-
-// // ------ EDIT GAME ------
-// function edit(key, gmName) {
-//     var gameName = prompt("Update the game name", gmName);
-//     if (gameName && gameName.length > 0) {
-//         // build the FB endpoint to the item in games list
-//         var updateGameRef = buildEndPoint(key);
-//         updateGameRef.update({
-//             name: gameName
-//         });
-//     }
-// }
- 
-// // ------ SAVE GAME ------
-// function del(key, gmName) {
-//     var response = confirm("Are certain about removing \"" + gmName + "\" from the list?");
-//     if (response == true) {
-//         // endpoint to the item in games list
-//         var deleteGameRef = buildEndPoint(key);
-//         deleteGameRef.remove();
-//     }
-// }
- 
-// function buildEndPoint (key) {
-// 	return new Firebase('https://high-score-app-3c45c.firebaseapp.com/' + key);
-// }
- 
-// // this will get fired on inital load as well as when ever there is a change in the data
-// myGames.on("value", function(snapshot) {
-//     var data = snapshot.val();
-//     var list = [];
-//     for (var key in data) {
-//         if (data.hasOwnProperty(key)) {
-//             name = data[key].name ? data[key].name : '';
-//             if (name.trim().length > 0) {
-//                 list.push({
-//                     name: name,
-//                     key: key
-//                 })
-//             }
-//         }
-//     }
-//     // refresh the UI
-//     refreshUI(list);
-// });
+// goes on load & whenever there's a data change
+myGames.on("value", function(snapshot) {
+    var data = snapshot.val();
+    var list = [];
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            name = data[key].name ? data[key].name : '';
+            if (name.trim().length > 0) {
+                list.push({
+                    name: name,
+                    key: key
+                })
+            }
+        }
+    }
+    // refresh the UI
+    refreshUI(list);
+});
